f4f1391d13f2081d1fdac857a59bd90c8f607d82
adithyaS1503 <121846151+adithyaS1503@users.noreply.github.com>
2024-09-11 20:17:09 +0530

    Delete key.py

--- key.py
+++
@@ -1,9 +0,0 @@
-from cryptography.fernet import Fernet
-
-# Generate and print the key
-key = Fernet.generate_key()
-print(f"Your key: {key.decode()}")
-
-# Save the key to a file (you can reuse this on both machines)
-with open('secret.key', 'wb') as key_file:
-    key_file.write(key)
